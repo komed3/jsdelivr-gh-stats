8a036ad241b2193abf00948a358a3dceb34a63cf
komed3 <pk97@outlook.de>
2026-04-04 10:26:19 +0200

    define time periods

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import type { Period } from './types';
+
+const PERIODS: { value: Period; label: string }[] = [
+    { value: 'week', label: 'Last 7 Days' },
+    { value: 'month', label: 'Last 30 Days' },
+    { value: 'quarter', label: 'Last 90 Days' },
+    { value: 'year', label: 'Last Year' },
+    { value: 's-month', label: 'Last Month' },
+    { value: 's-quarter', label: 'Last Quarter' },
+    { value: 's-year', label: 'Last Year (Static)' }
+];
+
 export default function App () {
     return ( <></> );
 }
