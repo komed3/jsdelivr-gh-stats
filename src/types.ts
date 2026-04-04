198ac60579f80d5bec803e82066c48e680339a2c
komed3 <pk97@outlook.de>
2026-04-04 10:18:39 +0200

    define types

---
+++ src/types.ts
@@ -0,0 +1,31 @@
+export interface StatsData {
+    rank: number | null;
+    typeRank: number | null;
+    total: number;
+    dates: Record<string, number>;
+    prev: {
+        rank: number | null;
+        typeRank: number | null;
+        total: number;
+    };
+}
+
+export interface JsDelivrResponse {
+    hits: StatsData;
+    bandwidth: StatsData;
+    links: {
+        self: string;
+        versions: string;
+    };
+}
+
+export type Period = 
+    | 'day' | 'week' | 'month' | 'quarter' | 'year'
+    | 's-month' | 's-quarter' | 's-year';
+
+export interface ChartDataPoint {
+    date: string;
+    hits: number;
+    bandwidth: number;
+    formattedDate: string;
+}
